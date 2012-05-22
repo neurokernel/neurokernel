d2be0a3bf4db47d1b35971784c42070c1f313eb9
hg <none@none>
2012-05-22 11:24:16 -0400

    Attempted relative import beyond toplevel package
    MockNetwork.py -> from .. import Module
    
    --HG--
    rename : Mock/Connectivity.py => Connectivity.py

--- Mock/MockNetwork.py
+++ Mock/MockNetwork.py
@@ -1,14 +1,12 @@
-import pycuda.gpuarray as garray
-import pycuda.driver as cuda
-import tools.parray as parray
 import numpy as np
 import random as rd
+import pycuda.gpuarray as garray
+import pycuda.driver as cuda
 from tools.simpleio import *
-from MorrisLecar import MorrisLecar
-from VectorSynapse import VectorSynapse
-from ..Module as Mod
+from tools.parray import *
+from .. import Module
 
-class MockNetwork (Mod):
+class MockNetwork (Module):
     """
     Neural network class. This code, by now, is provided by the user. In this
     example, this code is the lamina version implemented by Nikul and Yiyin.
@@ -18,7 +16,7 @@ class MockNetwork (Mod):
 
         np.random.seed(0)
 
-        Mod.__init__(self, manager, dt, num_in_non, num_in_spike, num_proj_non,
+        Module.__init__(self, manager, dt, num_in_non, num_in_spike, num_proj_non,
                  num_proj_spike, device)
 
         # In order to understand pre_neuron, post_neuron and dendrites it's
@@ -126,3 +124,201 @@ class CircularArray:
         if self.current >= self.delay_steps:
             self.current = 0
 
+# In Yiyin, this class is called 'vector_neurons'
+class MorrisLecar:
+    def __init__(self, num_neurons, num_types, num_cart, neuron_start, dt,
+                 num_dendrite, V, n, V1, V2, V3, V4, Tphi, offset,
+                 non_input_start):
+        """
+        Set Morris Lecar neurons in the network.
+
+        Parameters
+        ----------
+        N : int
+            Number of neurons to be added.
+        """
+        self.dtype = np.double
+        self.num_cart = num_cart
+        self.num_types = num_types
+        self.num_neurons = num_neurons
+        self.dt = dt
+        self.steps = max(int(round(dt / 1e-5)), 1)
+
+        self.ddt = dt / self.steps
+
+        self.V = garray.to_gpu(V)
+        self.n = garray.to_gpu(n)
+
+        self.I_pre = garray.zeros(self.num_neurons, np.double)
+
+        self.h_V = cuda.pagelocked_empty((self.num_types, self.num_cart),
+                                         np.double)
+
+        self.cum_num_dendrite = garray.to_gpu(np.concatenate((np.asarray([0, ],
+                                                dtype = np.int32),
+                                                np.cumsum(num_dendrite,
+                                                          dtype = np.int32))))
+        self.num_dendrite = garray.to_gpu(num_dendrite)
+        self.num_input = int(neuron_start[non_input_start])
+
+        self.update = self.get_euler_kernel(neuron_start, V1, V2, V3, V4,
+                                            Tphi, offset)
+        self.get_input = self.get_input_func()
+
+    def update_I_pre_input(self, I_ext):
+        cuda.memcpy_dtod(int(self.I_pre.gpudata), I_ext,
+                self.num_input * self.I_pre.dtype.itemsize)
+
+    def read_synapse(self, conductance, V_rev, st = None):
+        self.get_input.prepared_async_call(self.grid_get_input,
+                                           self.block_get_input, st,
+                                           conductance.gpudata,
+                                           self.cum_num_dendrite.gpudata,
+                                           self.num_dendrite.gpudata,
+                                           self.I_pre.gpudata, self.V.gpudata,
+                                           V_rev.gpudata)
+
+    def eval(self, buffer, st = None):
+        self.update.prepared_async_call(self.update_grid, self.update_block,
+                                        st, self.V.gpudata, self.n.gpudata,
+                                        int(buffer.buffer.gpudata) + \
+                                        buffer.current * buffer.buffer.ld * \
+                                        buffer.buffer.dtype.itemsize,
+                                        self.num_neurons, self.I_pre.gpudata,
+                                        self.ddt * 1000, self.steps)
+
+    def get_euler_kernel(self, neuron_start, V1, V2, V3, V4, Tphi, offset):
+        template = open('cuda_code/euler_kernel.cu', 'r')
+
+        dtype = self.dtype
+        scalartype = dtype.type if dtype.__class__ is np.dtype else dtype
+        self.update_block = (128, 1, 1)
+        self.update_grid = ((self.num_neurons - 1) / 128 + 1, 1)
+        mod = SourceModule(template.read() % {"type": dtype_to_ctype(dtype),
+                                              "ntype": self.num_types,
+                                              "nneu": self.update_block[0]},
+                           options = ["--ptxas-options=-v"])
+        func = mod.get_function("hhn_euler_multiple")
+
+        V1_addr, V1_nbytes = mod.get_global("V_1")
+        V2_addr, V2_nbytes = mod.get_global("V_2")
+        V3_addr, V3_nbytes = mod.get_global("V_3")
+        V4_addr, V4_nbytes = mod.get_global("V_4")
+        Tphi_addr, Tphi_nbytes = mod.get_global("Tphi")
+        neuron_start_addr, neuron_start_nbytes = mod.get_global("neuron_start")
+        offset_addr, offset_nbytes = mod.get_global("offset")
+
+        cuda.memcpy_htod(V1_addr, V1)
+        cuda.memcpy_htod(V2_addr, V2)
+        cuda.memcpy_htod(V3_addr, V3)
+        cuda.memcpy_htod(V4_addr, V4)
+        cuda.memcpy_htod(Tphi_addr, Tphi)
+        cuda.memcpy_htod(neuron_start_addr, neuron_start)
+        cuda.memcpy_htod(offset_addr, offset)
+
+        func.prepare([np.intp, np.intp, np.intp, np.int32, np.intp, scalartype,
+                      np.int32])
+
+        return func
+
+    def get_euler_kernel1(self, neuron_start, V1, V2, V3, V4, Tphi, offset):
+        template = open('cuda_code/euler_kernel1.cu', 'r')
+
+        dtype = self.dtype
+        scalartype = dtype.type if dtype.__class__ is np.dtype else dtype
+        mod = SourceModule(template.read() % {"type": dtype_to_ctype(dtype),
+                                              "ntype": self.num_types},
+                           options = ["--ptxas-options=-v"])
+        func = mod.get_function("hhn_euler_multiple")
+
+        V1_addr, V1_nbytes = mod.get_global("V_1")
+        V2_addr, V2_nbytes = mod.get_global("V_2")
+        V3_addr, V3_nbytes = mod.get_global("V_3")
+        V4_addr, V4_nbytes = mod.get_global("V_4")
+        Tphi_addr, Tphi_nbytes = mod.get_global("Tphi")
+        neuron_start_addr, neuron_start_nbytes = mod.get_global("neuron_start")
+        offset_addr, offset_nbytes = mod.get_global("offset")
+
+
+        cuda.memcpy_htod(V1_addr, V1)
+        cuda.memcpy_htod(V2_addr, V2)
+        cuda.memcpy_htod(V3_addr, V3)
+        cuda.memcpy_htod(V4_addr, V4)
+        cuda.memcpy_htod(Tphi_addr, Tphi)
+        cuda.memcpy_htod(neuron_start_addr, neuron_start)
+        cuda.memcpy_htod(offset_addr, offset)
+
+        func.prepare([np.intp, np.intp, np.intp, np.int32, np.intp, scalartype,
+                      np.int32])
+
+        self.update_block = (64, 2, 1)
+        self.update_grid = ((self.num_neurons - 1) / 64 + 1, 1)
+
+        return func
+
+    def get_input_func(self):
+        template = open('cuda_code/input_func.cu', 'r')
+
+        mod = SourceModule(template.read() % {"num_neurons": self.num_neurons},
+                           options = ["--ptxas-options=-v"])
+        func = mod.get_function("get_input")
+        func.prepare([np.intp, np.intp, np.intp, np.intp, np.intp, np.intp])
+        self.block_get_input = (32, 32, 1)
+        self.grid_get_input = ((self.num_neurons - 1) / 32 + 1, 1)
+
+        return func
+
+# Old name: vector_synapse
+
+class VectorSynapse:
+    def __init__(self, num_synapse, pre_neuron, post_neuron, syn_thres,
+                 syn_slope, syn_power, syn_saturation, syn_delay, V_rev, dt):
+        self.dt = dt
+        self.num_synapse = num_synapse
+        self.pre_neuron = garray.to_gpu(pre_neuron)
+        #self.post_neuron = garray.to_gpu(post_neuron)
+
+        self.threshold = garray.to_gpu(syn_thres)
+        self.slope = garray.to_gpu(syn_slope)
+        self.power = garray.to_gpu(syn_power)
+        self.saturation = garray.to_gpu(syn_saturation)
+        self.delay = garray.to_gpu(
+                            np.round(syn_delay * 1e-3 / dt).astype(np.int32))
+        self.conductance = garray.zeros(self.num_synapse, np.double)
+
+        self.V_rev = garray.to_gpu(V_rev)
+
+        self.update_terminal_synapse = self.get_update_terminal_synapse_func()
+        self.mem_tmp = garray.empty(self.num_synapse, np.double)
+
+    def compute_synapse(self, buffer, st = None):
+        self.update_terminal_synapse.prepared_async_call(
+                                                    self.grid_terminal_synapse,
+                                                    self.block_terminal_synapse,
+                                                    st, buffer.buffer.gpudata,
+                                                    buffer.buffer.ld,
+                                                    buffer.current,
+                                                    buffer.delay_steps,
+                                                    self.pre_neuron.gpudata,
+                                                    self.conductance.gpudata,
+                                                    self.threshold.gpudata,
+                                                    self.slope.gpudata,
+                                                    self.power.gpudata,
+                                                    self.saturation.gpudata,
+                                                    self.delay.gpudata,
+                                                    self.mem_tmp.gpudata)
+
+    def get_update_terminal_synapse_func(self):
+        template = open('cuda_code/terminal_synapse.cu', 'r')
+
+        mod = SourceModule(template.read() % {"n_synapse": self.num_synapse},
+                           options = ["--ptxas-options=-v"])
+        func = mod.get_function("update_terminal_synapse")
+        func.prepare([np.intp, np.int32, np.int32, np.int32, np.intp, np.intp,
+                      np.intp, np.intp, np.intp, np.intp, np.intp, np.intp])
+        self.block_terminal_synapse = (256, 1, 1)
+        self.grid_terminal_synapse = (min(6 * \
+                              cuda.Context.get_device().MULTIPROCESSOR_COUNT,
+                              (self.num_synapse - 1) / 256 + 1), 1)
+
+        return func
