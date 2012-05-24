4b5a529f2398122d0d43e554ce2721fd680a51fc
hg <none@none>
2012-05-24 16:21:08 -0400

    Almost all-random mock non-spiking example. Known issues: - Despite all neuron parameters and connections are being generate randomly, neurons are divided in 15 groups. That's because the earlier code based on the fruitfly lamina and it's neuron types.

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import neurokernel.Manager as Manager
 from neurokernel.MockSystem.MockSystem import MockSystem
 
 manager = Manager.Manager()
-manager.add_module(MockSystem(manager, 11520, 72218, 1e-4, 4608, 0, 4608, 0, 1))
+manager.add_module(MockSystem(manager, 768, 8, 1e-4, 4608, 0, 4608, 0, 1))
 
 manager.start()
--- neurokernel/Module.py
+++ neurokernel/Module.py
@@ -4,40 +4,33 @@ from neurokernel.tools import parray
 from multiprocessing import Process
 import numpy as np
 
-"""
-Can you please construct an object-oriented PyCUDA implementation of a network
-of ML neurons randomly connected by alpha function synapses (similar to the
-IAF network you implemented for E9070) that we can use for testing the
-architecture?
-I imagine that a standard numerical ODE solver such as low-order Runge-Kutta
-should be sufficient for simulating the network.
-Also, I'm not sure what dynamics we should expect to observe for such a
-network. You may need to talk to Yiyin or Nikul for further information about
-both of these points.
-"""
 class Module (Process):
 
     def __init__(self, manager, dt, num_in_non, num_in_spike, num_proj_non,
                  num_proj_spike, device):
         """
-        Interface between LPU and architecture.
+        A module comprises one or more local processing units and it is the
+        interface between those LPU and the manager.
             Parameters
             ----------
-            in_non_list : list of numpy.ndarray of numpy.float64
-                States (membrane voltages) of external non-spiking neurons
-                presynaptic to the module at the current time.
-            in_spike_list : list of numpy.ndarray of numpy.int32
-                Indices of external spiking neurons presynaptic to the module
-                that emitted a spike at the current time.
-            pro_non : numpy.ndarray of numpy.float64
-                States (membrane voltages) of non-spiking projec- tion neurons
-                at current time.
-            proj_spike : numpy.ndarray of numpy.int32
-                Indices of spiking projection neurons that emitted a spike at
+            manager : neurokernel.Manager
+                See Manager.py for more information
+            dt : numpy.double
+            num_in_non : int
+                Number of non-spiking neuron's states (membrane voltages)
+                of external non-spiking neurons presynaptic to the module at
                 the current time.
-            param : list
-                List of variables to configure the LPU.
-            dev : int
+            num_in_spike : int
+                Number of non-spiking neuron's indices of external spiking
+                neurons presynaptic to the module that emitted a spike at the
+                current time.
+            num_proj_non : int
+                Number of non-spiking neuron's states (membrane voltages) of
+                non-spiking projec- tion neurons at current time.
+            num_proj_spike : int
+                Number of non-spiking neuron's indices of spiking projection
+                neurons that emitted a spike at the current time.
+            device : int
                 Indicates which GPU device will be used by this module.
         """
 
@@ -55,9 +48,25 @@ class Module (Process):
         self.proj_spike = []
 
     def init_gpu(self):
+        """
+        Because the module can run as an independent process, the CUDA driver
+        is initialized in run-time and after the module has been initialized.
+        However, some variables are stored in GPU and need a CUDA context ready.
+        """
+
         raise NotImplementedError('You have to provide this method.')
 
     def run_step(self, in_non_list, in_spike_list, proj_non, proj_spike):
+        """
+        Runs one step of the module simulation.
+
+        Parameters
+            ----------
+            in_non_list : list of
+                See Manager.py for more information
+            dt : numpy.double
+            num_in_non : int
+        """
 
         raise NotImplementedError('You have to provide this method.')
 
