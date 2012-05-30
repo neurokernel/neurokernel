52b104cc936135bcd9abe65ba535a30cab60c80a
dchevitarese <none@none>
2012-05-30 10:52:18 -0400

    Username

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import neurokernel.Manager as Manager
 from neurokernel.MockSystem.MockSystem import MockSystem
 
 manager = Manager.Manager()
-manager.add_module(MockSystem(manager, 768, 8, 1e-4, 4608, 0, 4608, 0, 1))
+manager.add_module(MockSystem(manager, 1024, 8, 1e-4, 4608, 0, 4608, 0, 0))
+manager.add_module(MockSystem(manager, 2048, 8, 1e-4, 4608, 0, 4608, 0, 1))
 
 manager.start()
