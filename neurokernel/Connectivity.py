e4ad9c0de67d58e78663cf0a0e2ada086ef5cf64
Daniel Salles Chevitarese <dchevitarese@me.com>
2012-05-31 15:06:02 -0400

    Connectivity of non spiking neurons without parameters.

--- neurokernel/Connectivity.py
+++ neurokernel/Connectivity.py
@@ -1,10 +1,8 @@
 import numpy as np
 
-# We have to make a choice between apply every time the mask over the output
-# values for each module or send the entire output vector to each module.
 class Connectivity:
 
-    def __init__(self, mapping, proj_module):
+    def __init__(self, mapping, proj_module, input_module):
 
         # 2D array (bool) as shown below:
         #       out1  out2  out3  out4
@@ -18,9 +16,22 @@ class Connectivity:
             raise IOError, "You must provide a 2D numpy.darray"
         self.map = mapping
 
-        self.proj_module = proj_module
-        self.output_mask = self.map.sum(axis = 0).astype(np.bool)
-        self.compressed = np.compress(self.output_mask, self.map, axis = 1)
+        # support vector in the form:
+        # | True | False | False | True | True
+        # where True or False mean if it is necessary to transmit or not the
+        # state of determined neuron
+        map_support = self.map.sum(axis = 0).astype(np.bool)
+        # This mask contains the indices of neurons that will have their states
+        # transmitted to the other LPU and will be stored at the same GPU of
+        # the sender module.
+        self.output_mask = np.compress(map_support, map_support * \
+                                       range(1, len(map_support) + 1)) - 1
+        # The compressed matrices will be stored at the same GPU of the
+        # receiver module and represent the connectivity between the neurons
+        # that are indeed connected. E.g.: if one module has 100 projection
+        # neurons, but just 40 neurons are connected to a module, the matrices
+        # need to have a shape like (num_receivers, 40) to map the connections.
+        self.compressed_map = np.compress(map_support, self.map, axis = 1)
 
 #        self.syn_parameters
 
@@ -31,8 +42,8 @@ class Connectivity:
         self.connectivities.remove(connecivity)
 
     # Gets the output signal in the form (num_inputs, 1) and
-    def get_output(self):
-        return self.proj_module.neurons.V.get() * self.map
+    def get_output(self, module):
+        return module.neurons.V.get() * self.map
 
 def main():
     m = np.asarray([[0, 0, 1, 0, 1, 0, 1, 1, 0, 0],
