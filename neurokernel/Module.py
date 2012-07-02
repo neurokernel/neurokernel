65192f1ca2e4b1689bfcec084322ee2db4ace847
Lev Givon <lev@columbia.edu>
2012-07-02 14:37:14 -0400

    Clean up Module skeleton.
    --HG--
    branch : zmq

--- neurokernel/Module.py
+++ neurokernel/Module.py
@@ -7,7 +7,7 @@ from neurokernel.tools import parray
 from neurokernel.tools.comm_utils import is_poll_in
 from neurokernel.tools.gpu_utils import set_realloc
 
-class Module (mp.Process):
+class Module(mp.Process):
     """
     A module comprises one or more local processing units and it is the
     interface between those LPU and the manager.
@@ -18,20 +18,6 @@ class Module (mp.Process):
         Module manager that manages this module instance.
     dt : numpy.double
         Time resolution.
-    num_in_non : int
-        Number of non-spiking neuron's states (membrane voltages)
-        of external non-spiking neurons presynaptic to the module at
-        the current time.
-    num_in_spike : int
-        Number of non-spiking neuron's indices of external spiking
-        neurons presynaptic to the module that emitted a spike at the
-        current time.
-    num_proj_non : int
-        Number of non-spiking neuron's states (membrane voltages) of
-        non-spiking projec- tion neurons at current time.
-    num_proj_spike : int
-        Number of non-spiking neuron's indices of spiking projection
-        neurons that emitted a spike at the current time.
     device : int
         GPU device used by the module instance.
 
@@ -41,8 +27,7 @@ class Module (mp.Process):
     
     """
 
-    def __init__(self, dt, num_in_non, num_in_spike, num_proj_non,
-                 num_proj_spike, device, *args, **kwargs):
+    def __init__(self, dt, *args, **kwargs):
 
         # Module identifier for zmq communication:
         self.id = kwargs.pop('id')
@@ -53,12 +38,8 @@ class Module (mp.Process):
         self.logger = logging.getLogger('initializing module %s' % self.id)        
         Process.__init__(self)
 
-        self.running = True
         self.dt = dt
         self.device = device
-
-        self.num_in_non = num_in_non
-        self.num_in_spike = num_in_spike
 
         self.proj_non = []
         self.proj_spike = []
@@ -66,7 +47,7 @@ class Module (mp.Process):
         # List of connection objects:
         self.conn_list = []
 
-    def init_zmq(self):
+    def init_net(self):
         """
         Initialize ZeroMQ.
 
@@ -101,22 +82,22 @@ class Module (mp.Process):
         ctx = cuda.Device(self.device).make_context()
         atexit.register(ctx.pop)
         
-    def run_step(self, in_non_gpu, in_spike_gpu, out_non_gpu, out_spike_gpu):
+    def run_step(self, in_gpot_list, in_spike_list, out_gpot_gpu, out_gpot_gpu):
         """
         Run one step of the module simulation.
 
-        Each step of the module's simulation consumes the data in
-        `in_non_gpu` and `in_spike_gpu` and updates `out_non_gpu` and
-        `out_spike_gpu`.
+        Each step of the module's simulation consumes the data in the
+        arrays listed in `in_gpot_list` and `in_spike_list` and updates
+        `out_gpot_gpu` and `out_spike_gpu`.
         
         Parameters
         ----------
-        in_non_gpu : pycuda.gpuarray.GPUArray
-            States of external non-spiking input neurons.
-        in_spike_gpu : pycuda.gpuarray.GPUArray
+        in_gpot_list : list of pycuda.gpuarray.GPUArray
+            States of external graded-potential input neurons.
+        in_spike_list : list of pycuda.gpuarray.GPUArray
             Indices of external spiking neurons that produced a
             spike at the previous simulation step.
-        out_non_gpu : pycuda.gpuarray.GPUArray
+        out_gpot_gpu : pycuda.gpuarray.GPUArray
             States of non-spiking output neurons.
         out_spike_gpu : pycuda.gpuarray.GPUArray
             Indices of spiking output neurons that produce a spike at
@@ -126,19 +107,24 @@ class Module (mp.Process):
 
         raise NotImplementedError('You have to provide this method.')
 
-    def __sync(self, in_non_gpu, in_spike_gpu, out_non_gpu, out_spike_gpu):
+    def sync(self, in_gpot_list, in_spike_list, out_gpot_gpu, out_spike_gpu):
         """
         Propagate data to and from the module.
 
+        Given GPUArrays instantiated to contain input and output data
+        accessed by the module during a single step of simulation,
+        update the input arrays by receiving data from source modules
+        and send the data in the output arrays to destination modules.
+        
         Parameters
         ----------
-        in_non_gpu : pycuda.gpuarray.GPUArray
-            States of external non-spiking input neurons.
+        in_gpot_gpu : pycuda.gpuarray.GPUArray
+            States of external graded-potential input neurons.
         in_spike_gpu : pycuda.gpuarray.GPUArray
             Indices of external spiking neurons that produced a
             spike at the previous simulation step.
-        out_non_gpu : pycuda.gpuarray.GPUArray
-            States of non-spiking output neurons.
+        out_gpot_gpu : pycuda.gpuarray.GPUArray
+            States of graded-potential output neurons.
         out_spike_gpu : pycuda.gpuarray.GPUArray
             Indices of spiking output neurons that produce a spike at
             the current simulation step.
@@ -151,10 +137,10 @@ class Module (mp.Process):
             
         Notes
         -----
-        The non-spiking and spiking data arrays are transmitted
-        together in a tuple. 
+        The contents of the graded-potential and spiking data arrays
+        are transmitted together in a tuple.
 
-        The non-spiking data is an array if floats comprising the
+        The graded-potential data is an array of floats comprising the
         states of the external neurons. The spiking data is an array
         of integers listing the indices of the external neurons that
         have emitted a spike.
@@ -192,13 +178,13 @@ class Module (mp.Process):
         
         # Call initialization methods before simulation:
         self.init_gpu()
-        self.init_zmq()
+        self.init_net()
 
         # Main simulation loop:
         while True:
 
             # Propagate data between modules:
-            done = self.__sync(
+
             # Run a step of the simulation:
 
             # Check whether the simulation should terminate:
@@ -206,32 +192,3 @@ class Module (mp.Process):
         # Restore SIGINT signal handler before exiting:
         signal.signal(signal.SIGINT, orig_handler)
         self.logger.info('done')
-        
-        if self.num_in_non > 0:
-            self.in_non_list = parray.to_gpu(np.ones([1, self.num_in_non]))
-        else:
-            self.in_non_list = None
-        if self.num_in_spike > 0:
-            self.in_spike_list = parray.to_gpu(np.ones([1, self.num_in_spike]))
-        else:
-            self.in_spike_list = None
-
-            
-#        proj_non = np.empty((1, len(self.proj_non)), np.double)
-#        proj_spike = np.empty((1, len(self.proj_spike)), np.double)
-        dt = self.dt
-
-        I_ext = parray.to_gpu(np.ones([1 / dt, 4608]))
-        out = np.empty((1 / dt, 4608), np.double)
-
-        for i in range(int(1 / dt)):
-            self.run_step(int(I_ext.gpudata) + \
-                                        I_ext.dtype.itemsize * \
-                                        I_ext.ld * i, None, out[i, :], None)
-
-#        while(self.running):
-#            self.__run_step(self.in_non_list, self.in_spike_list, proj_non,
-#                       proj_spike)
-#            self.proj_non.append(proj_non)
-#            self.proj_spike.append(proj_spike)
-#            __sync()
