b2482acf4f02b503b925710b1505239b13fed91a
Lev Givon <lev@columbia.edu>
2012-07-10 14:43:27 -0400

    Add linger time, enable specification of what invokes the ExceptionOnSignal context manager.
    --HG--
    branch : kill_data_ctx

--- ctrl_proc.py
+++ ctrl_proc.py
@@ -24,34 +24,46 @@ import zmq
 from zmq.eventloop.ioloop import IOLoop
 from zmq.eventloop.zmqstream import ZMQStream
 
+# Use a finite linger time to prevent sockets from either hanging or
+# being uncleanly terminated when shutdown:
+LINGER_TIME = 2
+
 @contextmanager
-def ExceptionOnSignal(s=signal.SIGUSR1, e=Exception):
+def ExceptionOnSignal(s=signal.SIGUSR1, e=Exception, i=None):
     """
     Raise a specific exception when the specified signal is detected.
     """
 
     def handler(signum, frame):
-        raise e('signal %i detected' % s)
+        if i is not None:
+            raise e('signal %i detected in %s' % (s, i))
+        else:
+            raise e('signal %i detected' % s)
     signal.signal(s, handler)
     yield
 
 @contextmanager
-def TryExceptionOnSignal(s=signal.SIGUSR1, e=Exception):
+def TryExceptionOnSignal(s=signal.SIGUSR1, e=Exception, i=None):
     """
     Check for exception raised in response to specific signal.
     """
-    
-    with ExceptionOnSignal(s, e):
+
+    with ExceptionOnSignal(s, e, i):
         try:
             yield
         except e:
             pass
-    
+
+@contextmanager
+def NoExceptionOnSignal(s=signal.SIGUSR1):
+    signal.signal(s, signal.SIG_IGN)
+    yield
+
 class ControlledProcess(mp.Process):
     """
     Process subclass with control port.
     """
-    
+
     def __init__(self, port_ctrl, quit_sig, *args, **kwargs):
 
         # Use the object instance's Python ID (i.e., memory address)
@@ -59,7 +71,7 @@ class ControlledProcess(mp.Process):
         self.id = str(id(self))
 
         # Logging:
-        self.logger = twiggy.log.name(self.id)        
+        self.logger = twiggy.log.name(self.id)
 
         # Control port:
         self.port_ctrl = port_ctrl
@@ -67,28 +79,33 @@ class ControlledProcess(mp.Process):
         # Signal to use when quitting:
         self.quit_sig = quit_sig
         super(ControlledProcess, self).__init__(*args, **kwargs)
-        
+
     def _ctrl_handler(self, msg):
         """
         Control port handler.
         """
-        
+
         self.logger.info('recv: %s' % str(msg))
         if msg[0] == 'quit':
-            self.stream_ctrl.flush()
+            try:
+                self.stream_ctrl.flush()
+            except IOError:
+                self.logger.info('streams already closed')
+            self.stream_ctrl.stop_on_recv()
             self.logger.info('issuing signal %s' % self.quit_sig)
             os.kill(os.getpid(), self.quit_sig)
-            
+
     def _init_ctrl_handler(self):
         """
         Initialize control port handler.
         """
 
-        # Set the linger period to 0 to prevent hanging on unsent
-        # messages when shutting down:
+        # Set the linger period to prevent hanging on unsent messages
+        # when shutting down:
+        self.logger.info('starting ctrl handler')
         self.sock_ctrl = self.ctx.socket(zmq.DEALER)
         self.sock_ctrl.setsockopt(zmq.IDENTITY, self.id)
-        self.sock_ctrl.setsockopt(zmq.LINGER, 0)
+        self.sock_ctrl.setsockopt(zmq.LINGER, LINGER_TIME)
         self.sock_ctrl.connect('tcp://localhost:%i' % self.port_ctrl)
 
         self.ioloop_ctrl = IOLoop.instance()
@@ -100,15 +117,15 @@ class ControlledProcess(mp.Process):
         """
         Initialize network connection.
         """
-        
+
         self.ctx = zmq.Context()
         self._init_ctrl_handler()
-                
+
     def run(self):
         """
         Body of process.
         """
-        
+
         with TryExceptionOnSignal(self.quit_sig):
             self._init_net()
             while True:
@@ -120,7 +137,7 @@ if __name__ == '__main__':
                                          stream=sys.stdout)
     twiggy.emitters['*'] = twiggy.filters.Emitter(twiggy.levels.DEBUG,
                                                   True, output)
-    
+
     PORT_CTRL = 6001
     ctx = zmq.Context()
     sock = ctx.socket(zmq.ROUTER)
@@ -130,4 +147,4 @@ if __name__ == '__main__':
     p.start()
     time.sleep(3)
     sock.send_multipart([p.id, 'quit'])
-        
+
