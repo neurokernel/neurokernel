664adf91dc04df58d0a4c67bc4a40c99b05ae3c7
Lev Givon <lev@columbia.edu>
2013-02-12 18:54:53 -0500

    Add class for outputting twiggy logs to a zeromq PUB socket.

--- neurokernel/tools/comm_utils.py
+++ neurokernel/tools/comm_utils.py
@@ -1,4 +1,5 @@
 import zmq
+import twiggy
 
 def is_poll_in(sock, poller, timeout=100):
     """
@@ -11,3 +12,24 @@ def is_poll_in(sock, poller, timeout=100):
     else:
         return False
     
+class ZMQOutput(twiggy.outputs.Output):
+    """
+    Output messages to a ZeroMQ PUB socket.
+    """
+    
+    def __init__(self, addr, 
+                 format=None, close_atexit=True):
+        self.addr = addr
+        super(ZMQOutput, self).__init__(format, close_atexit)
+        
+    def _open(self):
+        self.ctx = zmq.Context()
+        self.sock = self.ctx.socket(zmq.PUB)
+        self.sock.bind('tcp://*:5000')
+
+    def _close(self):
+        self.sock.close()
+
+    def _write(self, x):
+        self.sock.send(x)
+
