99a78a17ef093dd56efa26a329e12d1ea8717a90
Lev Givon <lev@columbia.edu>
2013-03-07 17:34:20 -0500

    Add new connectivity object that uses sparse matrices.

---
+++ neurokernel/conn.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+
+"""
+Synaptic connectivity class.
+"""
+
+import string
+
+import numpy as np
+import scipy.sparse
+import scipy as sp
+
+class Connectivity(object):
+    """
+    Inter-LPU connectivity.
+
+    Stores the connectivity between two LPUs as a series of sparse matrices.
+    Every connection in an instance of the class has the following indices:
+
+    - source port ID
+    - destination port ID
+    - synapse number (when two neurons are connected by more than one neuron)
+    - direction ('+' for source to destination, '-' for destination to source)
+    - parameter name (the default is 'conn' for simple connectivity)
+    
+    """
+
+    def __init__(self, shape):
+        self.shape = shape
+
+        # All matrices are stored in this dict:
+        self._data = {}
+
+        # Keys corresponding to each connectivity direction are stored in the
+        # following lists:
+        self._keys_by_dir = {'+': [],
+                             '-': []}
+
+        # Create connectivity matrices for both directions:
+        key = self._make_key(0, '+', 'conn')
+        self._data[key] = self._make_matrix(shape, int)
+        self._keys_by_dir['+'].append(key)        
+        key = self._make_key(0, '-', 'conn')
+        self._data[key] = self._make_matrix(shape, int)
+        self._keys_by_dir['-'].append(key)
+
+    @property
+    def N_src(self):
+        """
+        Number of source ports.
+        """
+        
+        return self.shape[0]
+
+    @property
+    def N_dest(self):
+        """
+        Number of destination ports.
+        """
+        
+        return self.shape[1]
+
+    @property
+    def nbytes(self):
+        """
+        Approximate number of bytes required by the class instance.
+
+        Notes
+        -----
+        Only accounts for nonzero values in sparse matrices.
+        """
+
+        count = 0
+        for key in self._data.keys():
+            count += self._data[key].dtype.itemsize*self._data[key].nnz
+        return count
+    
+    def _format_bin_array(self, a, indent=0):
+        """
+        Format a binary array for printing.
+        
+        Notes
+        -----
+        Assumes a 2D array containing binary values.
+        """
+        
+        sp0 = ' '*indent
+        sp1 = sp0+' '
+        a_list = a.toarray().tolist()
+        if a.shape[0] == 1:
+            return sp0+str(a_list)
+        else:
+            return sp0+'['+str(a_list[0])+'\n'+''.join(map(lambda s: sp1+str(s)+'\n', a_list[1:-1]))+sp1+str(a_list[-1])+']'
+        
+    def __repr__(self):
+        result = 'src -> dest\n'
+        result += '-----------\n'
+        for key in self._keys_by_dir['+']:
+            result += key + '\n'
+            result += self._format_bin_array(self._data[key]) + '\n'
+        result += '\ndest -> src\n'
+        result += '-----------\n'
+        for key in self._keys_by_dir['-']:
+            result += key + '\n'
+            result += self._format_bin_array(self._data[key]) + '\n'
+        return result
+        
+    def _make_key(self, syn, dir, param):
+        """
+        Create a unique key for a matrix of synapse properties.
+        """
+        
+        return string.join(map(str, [syn, dir, param]), '/')
+
+    def _make_matrix(self, shape, dtype=np.double):
+        """
+        Create a sparse matrix of the specified shape.
+        """
+        
+        return sp.sparse.lil_matrix(shape, dtype=dtype)
+            
+    def get(self, source, dest, syn=0, dir='+', param='conn'):
+        """
+        Retrieve a value in the connectivity class instance.
+        """
+
+        assert type(syn) == int
+        assert dir in ['-', '+']
+        
+        result = self._data[self._make_key(syn, dir, param)][source, dest]
+        if not np.isscalar(result):
+            return result.toarray()
+        else:
+            return result
+
+    def set(self, source, dest, syn=0, dir='+', param='conn', val=1):
+        """
+        Set a value in the connectivity class instance.
+
+        Notes
+        -----
+        Creates a new storage matrix when the one specified doesn't exist.        
+        """
+
+        assert type(syn) == int
+        assert dir in ['-', '+']
+        
+        key = self._make_key(syn, dir, param)
+        if not self._data.has_key(key):
+            self._data[key] = self._make_matrix(self.shape, type(val))
+            self._keys_by_dir[dir].append(key)
+        self._data[key][source, dest] = val
+
+    def flip(self):
+        """
+        Returns an object instance with the source and destination LPUs flipped.
+        """
+
+        c = Connectivity(self.shape[::-1])
+        for old_key in self._data.keys():
+
+            # Reverse the direction in the key:
+            key_split = old_key.split('/')
+            old_dir = key_split[1]
+            if old_dir == '+':
+                new_dir = '-'
+            elif old_dir == '-':
+                new_dir = '+'
+            else:
+                raise ValueError('invalid direction in key')    
+            key_split[1] = new_dir
+            new_key = '/'.join(key_split)
+            c._data[new_key] = self._data[old_key].T
+            c._keys_by_dir[new_dir].append(new_key)
+        return c
+        
+    def __getitem__(self, s):        
+        return self.get(*s)
+
+    def __setitem__(self, s, val):
+        self.set(*s, val=val)
+        
+if __name__ == '__main__':
+    pass
+
