0a6857e28a41476e2c1604c1afe0eb6dc9a7ba25
Lev Givon <lev@columbia.edu>
2013-03-12 19:21:29 -0400

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import sys, os
+from glob import glob
+
+# Install setuptools if it isn't available:
+try:
+    import setuptools
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+
+from distutils.command.install import INSTALL_SCHEMES
+from distutils.command.install_headers import install_headers
+from setuptools import find_packages
+from setuptools import setup
+
+NAME =               'neurokernel'
+VERSION =            '0.01'
+AUTHOR =             'Lev Givon'
+AUTHOR_EMAIL =       'lev@columbia.edu'
+URL =                'http://github.com/lebedov/scikits.cuda/'
+DESCRIPTION =        'An open architecture for Drosophila brain emulation'
+LONG_DESCRIPTION =   DESCRIPTION
+DOWNLOAD_URL =       URL
+LICENSE =            'BSD'
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Software Development']
+NAMESPACE_PACKAGES = ['neurokernel']
+PACKAGES =           find_packages()
+
+if __name__ == "__main__":
+    if os.path.exists('MANIFEST'): 
+        os.remove('MANIFEST')
+
+    # This enables the installation of neurokernel/__init__.py as a data
+    # file:
+    for scheme in INSTALL_SCHEMES.values():
+        scheme['data'] = scheme['purelib']
+
+    setup(
+        name = NAME,
+        version = VERSION,
+        author = AUTHOR,
+        author_email = AUTHOR_EMAIL,
+        license = LICENSE,
+        classifiers = CLASSIFIERS,
+        description = DESCRIPTION,
+        long_description = LONG_DESCRIPTION,
+        url = URL,
+        namespace_packages = NAMESPACE_PACKAGES,
+        packages = PACKAGES,
+
+        # Force installation of __init__.py in namespace package:
+        data_files = [('neurokernel', ['neurokernel/__init__.py'])],
+        include_package_data = True,
+        install_requires = [
+            'bidict',
+            'la',
+            'msgpack_numpy',
+            'numpy',
+            'pycuda >= 2012.1',
+            'pyzmq',
+            'scipy',
+            'twiggy'],
+        )
