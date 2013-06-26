4e905699463d8b7a9541a457d55ca1fd6822c82e
Lev Givon <lev@columbia.edu>
2013-06-26 16:10:51 -0400

    Add catch_exception() diagnostic method to tools.misc.

--- neurokernel/tools/misc.py
+++ neurokernel/tools/misc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import numpy as np
+import sys, traceback
 
 def rand_bin_matrix(sh, N, dtype=np.double):
     """
@@ -22,3 +23,35 @@ def rand_bin_matrix(sh, N, dtype=np.double):
     np.random.shuffle(indices)
     result.ravel()[indices[:N]] = 1
     return result
+
+def catch_exception(func, disp, *args, **kwargs):
+    """
+    Catch and report exceptions when executing a function.
+
+    If an exception occurs while executing the specified function, the
+    exception's message and the line number where it occurred (in the innermost
+    traceback frame) are displayed.
+    
+    Parameters
+    ----------
+    func : function
+        Function to execute. 
+    disp : function
+        Function to use to display exception message.
+    args : list
+        Function arguments.
+    kwargs : dict
+        Named function arguments.
+        
+    """
+    try:
+        func(*args, **kwargs)
+    except Exception as e:
+        
+        # Find the line number of the innermost traceback frame:
+        for fname in traceback.extract_tb(sys.exc_info()[2]):
+            fname, lineno, fn, text = fname
+
+        disp(func.__name__ + ': ' + e.message + \
+           ' (' + fname + ':' + str(lineno) + ')')
+             
