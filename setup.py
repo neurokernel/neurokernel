7ed9a7eb9483263aa1bc60bd76c743b4dd430aef
Lev Givon <lev@columbia.edu>
2013-12-25 21:50:32 -0500

    Add h5py as install dep.

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ if __name__ == "__main__":
         install_requires = [
             'bidict',
             'bottleneck',
+            'h5py',
             'la',
             'msgpack_numpy',
             'networkx',
