38350ccc978126d364ff051f1a52bf26d44d9728
Yiyin Zhou <yiyin@ee.columbia.edu>
2014-01-31 15:55:14 -0500

    Updated vision example and notebook

--- examples/vision/visualize_output.py
+++ examples/vision/visualize_output.py
@@ -16,6 +16,7 @@ conf_input['type'] = 'image'
 conf_input['clim'] = [0, 0.02]
 conf_input['ids'] = [range(32*24)]
 conf_input['shape'] = [32, 24]
+conf_input['trans'] = True
 
 V.add_LPU('data/vision_input.h5', LPU='Vision')
 V.add_plot(conf_input, 'input_Vision')
@@ -24,11 +25,13 @@ conf_lam_R1 = {}
 conf_lam_R1['type'] = 'image'
 conf_lam_R1['clim'] = [-0.055,-0.02]
 conf_lam_R1['shape'] = [32, 24]
+conf_lam_R1['trans'] = True
 
 conf_lam_L1 = {}
 conf_lam_L1['type'] = 'image'
 conf_lam_L1['clim'] = [-0.053,-0.047]
 conf_lam_L1['shape'] = [32, 24]
+conf_lam_L1['trans'] = True
 
 V.add_LPU('lamina_output_gpot.h5', './data/lamina.gexf.gz', 'Ret/Lam')
 V.add_plot(conf_lam_R1, 'Ret/Lam', 'R1')
@@ -40,8 +43,8 @@ conf_med['clim'] = [-0.052, -0.050]
 V.add_LPU('medulla_output_gpot.h5', './data/medulla.gexf.gz', 'Medulla')
 V.add_plot(conf_med, 'Medulla', 'T5a')
 
-V.rows = 1
-V.cols = 4
+V.rows = 2
+V.cols = 2
 V.fontsize = 22
 V.update_interval = 50
 V.out_filename = 'vision_output.avi'
