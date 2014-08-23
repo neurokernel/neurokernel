27210688ee2a221c6d844066c40e2b98744dc170
Nikul Ukani <nikul@e.columbia.edu>
2014-08-23 15:03:36 -0400

    Correct typos

--- examples/retina_lamina/data/models/gen_synapse.py
+++ examples/retina_lamina/data/models/gen_synapse.py
@@ -21,6 +21,6 @@ num_synapse_types = len(synapse_list)
 for i in range(num_synapse_types):
     print "    {'prename':'"+synapse_list[i][0]+"', 'postname':'"+synapse_list[i][1]+"', 'model':'power_gpot_gpot_sig',"
     print "    'cart':"+str(None if synapse_list[i][10] == 0 else synapse_list[i][10]) +", 'reverse':"+str(synapse_list[i][5])+", 'delay':"+str(synapse_list[i][4])+","
-    print "    'threshold':"+str(synapse_list[i][8])+", 'slope':"+str(synapse_list[i][3])+"', 'power':"+str(synapse_list[i][2])+", 'saturation':"+str(synapse_list[i][9])+","
+    print "    'threshold':"+str(synapse_list[i][8])+", 'slope':"+str(synapse_list[i][3])+", 'power':"+str(synapse_list[i][2])+", 'saturation':"+str(synapse_list[i][9])+","
     print "    'scale':"+str(synapse_list[i][6])+", 'mode':0},"
 
