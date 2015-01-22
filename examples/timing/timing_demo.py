c32aa6c328cbf9761d4855657a49bda68bf4404d
Lev Givon <lev@columbia.edu>
2015-01-22 16:17:43 -0500

    Modify timing demo to work properly with validated_selector_class updates.

--- examples/timing/timing_demo.py
+++ examples/timing/timing_demo.py
@@ -14,7 +14,7 @@ import pycuda.driver as drv
 from neurokernel.base import setup_logger
 from neurokernel.core import Manager, Module, PORT_DATA, PORT_CTRL, PORT_TIME
 from neurokernel.pattern import Pattern
-from neurokernel.plsel import PathLikeSelector
+from neurokernel.plsel import Selector, SelectorMethods
 from neurokernel.tools.comm import get_random_port
 
 class MyModule(Module):
@@ -33,24 +33,23 @@ class MyModule(Module):
                  columns=['interface', 'io', 'type'],
                  port_data=PORT_DATA, port_ctrl=PORT_CTRL, port_time=PORT_TIME,
                  id=None, device=None, debug=False):
-        sel_gpot = ','.join([sel_in_gpot, sel_out_gpot])
-        sel_spike = ','.join([sel_in_spike, sel_out_spike])
+        sel_gpot = Selector(sel_in_gpot)+Selector(sel_out_gpot)
+        sel_spike = Selector(sel_in_spike)+Selector(sel_out_spike)
         if data_gpot is None:
-            data_gpot = np.zeros(PathLikeSelector.count_ports(sel_gpot), float)
+            data_gpot = np.zeros(SelectorMethods.count_ports(sel_gpot), float)
         if data_spike is None:
-            data_spike = np.zeros(PathLikeSelector.count_ports(sel_spike), int)
-
+            data_spike = np.zeros(SelectorMethods.count_ports(sel_spike), int)
         super(MyModule, self).__init__(sel, sel_gpot, sel_spike,
                                        data_gpot, data_spike,
                                        columns, port_data, port_ctrl, port_time,
                                        id, device, debug, True)
 
-        assert PathLikeSelector.is_in(sel_in_gpot, sel)
-        assert PathLikeSelector.is_in(sel_out_gpot, sel)
-        assert PathLikeSelector.are_disjoint(sel_in_gpot, sel_out_gpot)
-        assert PathLikeSelector.is_in(sel_in_spike, sel)
-        assert PathLikeSelector.is_in(sel_out_spike, sel)
-        assert PathLikeSelector.are_disjoint(sel_in_spike, sel_out_spike)
+        assert SelectorMethods.is_in(sel_in_gpot, sel)
+        assert SelectorMethods.is_in(sel_out_gpot, sel)
+        assert SelectorMethods.are_disjoint(sel_in_gpot, sel_out_gpot)
+        assert SelectorMethods.is_in(sel_in_spike, sel)
+        assert SelectorMethods.is_in(sel_out_spike, sel)
+        assert SelectorMethods.are_disjoint(sel_in_spike, sel_out_spike)
 
         self.interface[sel_in_gpot, 'io', 'type'] = ['in', 'gpot']
         self.interface[sel_out_gpot, 'io', 'type'] = ['out', 'gpot']
@@ -109,9 +108,9 @@ def gen_sels(n_lpu, n_spike, n_gpot):
         sel_out_spike = '/%s/out/spike/%s/[0:%i]' % (lpu_id,
                                                      other_lpu_ids,
                                                      n_spike)
-        results[lpu_id] = (sel_in_gpot, sel_in_spike,
-                           sel_out_gpot, sel_out_spike)
-                           
+        results[lpu_id] = (Selector(sel_in_gpot), Selector(sel_in_spike),
+                           Selector(sel_out_gpot), Selector(sel_out_spike))
+
     return results
 
 def emulate(n_lpu, n_spike, n_gpot, steps):
@@ -135,7 +134,17 @@ def emulate(n_lpu, n_spike, n_gpot, steps):
         have 2*n_gpot*(n_lpu-1) total graded potential ports.
     steps : int
         Number of steps to execute.
+
+    Returns
+    -------
+    throughput : float
+        Received data throughput in bytes/seconds.
+    exec_time : float
+        Execution time in seconds.
     """
+
+    # Time everything starting with manager initialization:
+    start = time.time()
 
     # Check whether a sufficient number of GPUs are available:
     drv.init()
@@ -149,10 +158,10 @@ def emulate(n_lpu, n_spike, n_gpot, steps):
     sel_dict = gen_sels(n_lpu, n_spike, n_gpot)
     for i in xrange(n_lpu):
         lpu_i = 'lpu%s' % i
-        sel = ','.join(sel_dict[lpu_i])
         sel_in_gpot, sel_in_spike, sel_out_gpot, sel_out_spike = sel_dict[lpu_i]
+        sel = sel_in_gpot+sel_in_spike+sel_out_gpot+sel_out_spike
         m = MyModule(sel,
-                     sel_in_gpot, sel_in_spike, sel_out_gpot, sel_out_spike,
+                sel_in_gpot, sel_in_spike, sel_out_gpot, sel_out_spike,
                      port_data=man.port_data, port_ctrl=man.port_ctrl,
                      port_time=man.port_time,
                      id=lpu_i, device=i, debug=args.debug)
@@ -166,10 +175,12 @@ def emulate(n_lpu, n_spike, n_gpot, steps):
             sel_dict[lpu_i]
         sel_in_gpot_j, sel_in_spike_j, sel_out_gpot_j, sel_out_spike_j = \
             sel_dict[lpu_j]
-        sel_from = sel_out_gpot_i+','+sel_out_spike_i+','+sel_out_gpot_j+','+sel_out_spike_j
-        sel_to = sel_in_gpot_j+','+sel_in_spike_j+','+sel_in_gpot_i+','+sel_in_spike_i
+        sel_from = sel_out_gpot_i+sel_out_spike_i+sel_out_gpot_j+sel_out_spike_j
+        sel_to = sel_in_gpot_j+sel_in_spike_j+sel_in_gpot_i+sel_in_spike_i
+        man.log_info('before from_concat')
         pat = Pattern.from_concat(sel_from, sel_to,
                                   from_sel=sel_from, to_sel=sel_to, data=1)
+        man.log_info('before setting attribs')
         pat.interface[sel_in_gpot_i] = [0, 'out', 'gpot']
         pat.interface[sel_out_gpot_i] = [0, 'in', 'gpot']
         pat.interface[sel_in_spike_i] = [0, 'out', 'spike']
@@ -178,12 +189,12 @@ def emulate(n_lpu, n_spike, n_gpot, steps):
         pat.interface[sel_out_gpot_j] = [1, 'in', 'gpot']
         pat.interface[sel_in_spike_j] = [1, 'out', 'spike']
         pat.interface[sel_out_spike_j] = [1, 'in', 'spike']
+        man.log_info('before connecting modules')
         man.connect(man.modules[lpu_i], man.modules[lpu_j], pat, 0, 1)
- 
-    #start = time.time()
+
     man.start(steps=steps)
     man.stop()
-    #print 'run time: %s' % (time.time()-start)
+    return man.get_throughput(), (time.time()-start)
 
 if __name__ == '__main__':
     num_lpus = 2
@@ -213,7 +224,6 @@ if __name__ == '__main__':
         file_name = 'neurokernel.log'
     if args.log.lower() in ['screen', 'both']:
         screen = True
-    logger = setup_logger(file_name=file_name, screen=screen)
+    logger = setup_logger(file_name=file_name, screen=screen, multiline=True)
 
-    emulate(args.num_lpus, args.num_spike, args.num_gpot, args.max_steps)
-
+    print emulate(args.num_lpus, args.num_spike, args.num_gpot, args.max_steps)
