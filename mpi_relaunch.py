be6e850c2589ff1c3e8bb9a0b3df2aefebbb2422
Lev Givon <lev@columbia.edu>
2015-02-28 22:29:58 -0500

    Prevent SLURM from clashing with self-launch; raise less confusing exception when mpiexec cannot be executed.

--- mpi_relaunch.py
+++ mpi_relaunch.py
@@ -14,15 +14,27 @@ import sys
 from mpi4py import MPI
 import psutil
 
+# Prevent SLURM from preventing mpiexec from starting multiple processes
+# (this approach should probably be modified to take some SLURM variables into
+# consideration):
+env = os.environ.copy()
+for k in env.keys():
+    if k.startswith('SLURM'):
+        del env[k]
+
 # Get name of the file in which this module is imported:
 script_name = os.path.basename(inspect.stack()[1][1])
 parent_name = psutil.Process(os.getppid()).name()
 if not re.search('mpirun|mpiexec', parent_name):
-    subprocess.call(['mpiexec', '-np', '1',
-                    sys.executable, script_name],
-                    env=os.environ,
-                    stdout=sys.stdout,
-                    stderr=sys.stderr,
-                    stdin=sys.stdin)
-    sys.exit(0)
+    try:
+        subprocess.call(['mpiexec', '-np', '1',
+                        sys.executable, script_name],
+                        env=env,
+                        stdout=sys.stdout,
+                        stderr=sys.stderr,
+                        stdin=sys.stdin)
+    except IOError:
+        raise IOError('cannot execute mpiexec')
+    else:
+        sys.exit(0)
 
