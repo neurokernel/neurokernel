1270b7323a5a3d67c879bb09b42258f68120c8f4
Lev Givon <lev@columbia.edu>
2015-03-03 16:29:58 -0500

    Transmit process classes to spawned processes instead of trying to import them, also transmit required globals, use dill for obj serialization, work around dill bug #81, use logger mixin in Process/ProcessManager class defs.

--- neurokernel/mpi_backend.py
+++ neurokernel/mpi_backend.py
@@ -4,39 +4,63 @@
 Backend program invoked by MPI spawn.
 """
 
-import importlib
 import inspect
 import os.path
 import sys
 
+# Use dill for mpi4py object serialization to accomodate a wider range of argument
+# possibilities than possible with pickle:
 import dill
+
+# Fix for bug https://github.com/uqfoundation/dill/issues/81
+@dill.register(property)
+def save_property(pickler, obj):
+    pickler.save_reduce(property, (obj.fget, obj.fset, obj.fdel), obj=obj)
+
+import twiggy
 from mpi4py import MPI
 MPI.pickle.dumps = dill.dumps
 MPI.pickle.loads = dill.loads
 
+# Need to import tools.comm so that the MPIOutput class in it can be used to
+# find instances thereof in the transmitted emitters:
+import tools.comm
+
 # Process needs to be imported directly into the script's namespace in order to
 # ensure that the issubclass() check later in the script succeeds:
 from neurokernel.mpi_proc import Process
+import neurokernel.mpi_proc
 
 size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
 parent = MPI.Comm.Get_parent()
 
-# Import the module containing the various classes/functions that must be run:
-mod_name = os.path.splitext(os.path.basename(sys.argv[1]))[0]
-m = importlib.import_module(mod_name)
+# Get emitters transmitted from spawning process:
+emitters = parent.recv()
 
-# Get the target class/function and its constructor arguments 
-# (in principle, the class could be serialized and transmitted rather than
-# imported):
-name, args, kwargs = parent.recv()
+# If any of the emitters contain MPIOutput instances, they need to be replaced
+# by newly initialized instances so that they write to valid file handles and
+# use the intercommunicator to the parent process:
+for k, v in emitters.iteritems():
+    if isinstance(v._output, tools.comm.MPIOutput):
+        level = v.min_level
+        name = v._output.filename
+        format = v._output._format
+        mode = v._output.mode
+        # XXX what about close_atexit? XXX        
+        twiggy.addEmitters(('file', level, None, 
+                            tools.comm.MPIOutput(name, format, MPI.COMM_WORLD, mode)))
+    else:
+        twiggy.emitters[k] = v
 
-# Target is a class:
-target = getattr(m, name)
-if inspect.isclass(target) and issubclass(target, Process):
-    instance = target(*args, **kwargs)
-    instance.run()
+# Get the target class/function and its constructor arguments:
+target, target_globals, args, kwargs = parent.recv()
 
-# Target is a function:
-else:
-    target(*args, **kwargs)
+# Insert the transmitted globals into the current scope:
+globals()[target.__name__] = target
+for k, n in target_globals.iteritems():
+    globals()[k] = n
+
+# Instantiate and run the target class:
+instance = target(*args, **kwargs)
+instance.run()
--- neurokernel/mpi_proc.py
+++ neurokernel/mpi_proc.py
@@ -11,18 +11,76 @@ import sys
 # Use dill for mpi4py object serialization to accomodate a wider range of argument
 # possibilities than possible with pickle:
 import dill
+
+# Fix for bug https://github.com/uqfoundation/dill/issues/81
+@dill.register(property)
+def save_property(pickler, obj):
+    pickler.save_reduce(property, (obj.fget, obj.fset, obj.fdel), obj=obj)
+
+import twiggy
 from mpi4py import MPI
 MPI.pickle.dumps = dill.dumps
 MPI.pickle.loads = dill.loads
 
+from mixins import LoggerMixin
+from tools.logging import set_excepthook
 from tools.misc import memoized_property
 
-class Process(object):
+def funcvars(x):
+    """
+    Find objects accessed by a function or method.
+    """
+
+    # Find symbols accessed by specified function or method:
+    results = {}
+    if inspect.isfunction(x):
+        name_list = x.func_code.co_names
+        global_dict = x.func_globals
+    elif inspect.ismethod(x):
+        name_list = x.im_func.func_code.co_names
+        global_dict = x.im_func.func_globals
+    else:
+        raise ValueError('invalid input')
+    for name in name_list:
+
+        # Symbol is a name of a global:
+        if name in global_dict:
+            results[name] = global_dict[name]
+        else:
+
+            # Check if symbol is the name is an attribute of a module (i.e., can
+            # be imported):
+            for r in results.keys():
+                if hasattr(results[r], name) and \
+                   inspect.ismodule(getattr(results[r], name)):
+                    results[r+'.'+name] = getattr(results[r], name)
+    return results
+
+def allglobalvars(x):
+    """
+    Find all globals accessed by an object.
+    """
+
+    results = {}
+    if inspect.isroutine(x):
+        results = funcvars(x)
+    else:
+        if inspect.isclass(x):
+            for b in x.__bases__:
+                results.update(allglobalvars(b))
+        for f in inspect.getmembers(x, predicate=inspect.ismethod):
+            results.update(allglobalvars(f[1]))
+    return results
+
+class Process(LoggerMixin):
     """
     Process class.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):        
+        LoggerMixin.__init__(self, 'prc %s' % MPI.COMM_WORLD.Get_rank())
+        set_excepthook(self.logger, True)
+
         self._args = args
         self._kwargs = kwargs
 
@@ -89,12 +147,15 @@ class Process(object):
     def recv_peer(self, source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG):
         return self.intracomm.recv(source=source, tag=tag)
 
-class ProcessManager(object):
+class ProcessManager(LoggerMixin):
     """
     Process manager class.
     """
 
     def __init__(self):
+        LoggerMixin.__init__(self, 'man')
+        set_excepthook(self.logger, True)
+
         self._targets = []
         self._args = []
         self._kwargs = []
@@ -118,16 +179,15 @@ class ProcessManager(object):
 
         Parameters
         ----------
-        target : Process or function
-            Class or function to instantiate in MPI process. If a class is
-            specified, the `run()` method of the class will be executed in the
-            process.
+        target : Process
+            Class instantiate and run in MPI process. 
         args : sequence
             Sequential arguments to pass to target class constructor.
         kwargs : dict
             Named arguments to pass to target class constructor.
         """
 
+        assert issubclass(target, Process)
         self._targets.append(target)
         self._args.append(args)
         self._kwargs.append(kwargs)
@@ -163,11 +223,18 @@ class ProcessManager(object):
                                             args=[mpi_backend_path, file_name],
                                             maxprocs=len(self))
 
-            # Transmit class name, args, and kwargs; the
-            # backend will wait to receive them and then start running the
-            # targets on the appropriate nodes.
+            # First, transmit twiggy logging emitters to spawned processes so
+            # that they can configure their logging facilities:
             for i in xrange(len(self)):
-                data = (self._targets[i].__name__, self._args[i], self._kwargs[i])
+                self._intercomm.send(twiggy.emitters, i)
+
+            # Transmit class to instantiate, globals required by the class, and
+            # the constructor args, and kwargs; the backend will wait to receive
+            # them and then start running the targets on the appropriate nodes.
+            for i in xrange(len(self)):
+                target_globals = allglobalvars(self._targets[i])
+                data = (self._targets[i], target_globals, 
+                        self._args[i], self._kwargs[i])      
                 self._intercomm.send(data, i)
 
     def send(self, data, dest, tag=0):
@@ -183,3 +250,21 @@ class ProcessManager(object):
         """
 
         return self.intercomm.recv(tag=tag)
+
+if __name__ == '__main__':
+    class MyProcess(Process):
+        def __init__(self, *args, **kwargs):
+            super(MyProcess, self).__init__(*args, **kwargs)
+            self.log_info('I am process %d of %d on %s.' % \
+                          (MPI.COMM_WORLD.Get_rank(),
+                           MPI.COMM_WORLD.Get_size(),
+                           MPI.COMM_WORLD.Get_name()))
+        
+    from tools.logging import setup_logger
+
+    setup_logger(screen=True, multiline=True)
+    
+    man = ProcessManager()
+    man.add(MyProcess, 1, 2, a=3)
+    man.add(MyProcess, 4, b=5, c=6)
+    man.run()
