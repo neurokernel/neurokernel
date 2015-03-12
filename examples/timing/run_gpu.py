f0ae7940ad8224a0cda4fbfeb327c7cc0157250a
Lev Givon <lev@columbia.edu>
2015-03-12 18:38:45 -0400

    Tweak timing demo run scripts.

--- examples/timing/run_gpu.py
+++ examples/timing/run_gpu.py
@@ -14,13 +14,13 @@ import sys
 script_name = 'timing_demo_gpu.py'
 
 w = csv.writer(sys.stdout)
-for spikes in np.linspace(100, 10000, 10, dtype=int):
+for spikes in np.linspace(500, 15000, 10, dtype=int):
     average_throughput_list = []
     total_throughput_list = []
     runtime_list = []
-    for i in xrange(3):
+    for i in xrange(2):
         out = subprocess.check_output(['python', script_name,
-                        '-u', '2', '-s', str(spikes), '-g', '0', '-m', '500'])
+                        '-u', '2', '-s', str(spikes), '-g', '0', '-m', '100'])
         average_throughput, total_throughput, runtime = out.strip('()\n\"').split(', ')
         average_throughput_list.append(float(average_throughput))
         total_throughput_list.append(float(total_throughput))
