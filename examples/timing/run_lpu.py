c7ce51239cd8fe02c4379c258e1df9dcc610828d
Lev Givon <lev@columbia.edu>
2015-03-31 17:57:21 -0400

    Run timing demos over wider range of port # values.

--- examples/timing/run_lpu.py
+++ examples/timing/run_lpu.py
@@ -4,40 +4,28 @@
 Run timing test (non-GPU) scaled over number of LPUs.
 """
 
-import numpy as np
-
 import csv
 import re
 import subprocess
 import sys
 
-script_name = 'timing_demo.py'
+import numpy as np
 
-w = csv.writer(sys.stdout)
-for lpus in xrange(2, 9):
-    average_step_sync_time_list = []
-    average_throughput_list = []
-    total_throughput_list = []
-    runtime_all_list = []
-    runtime_main_list = []
-    runtime_loop_list = []
-    for i in xrange(2):
-        out = subprocess.check_output(['srun', '-n', '1', '-c', str(lpus),
-                                       'python', script_name,
-                                       '-u', str(lpus), '-s', '1000', '-g', '0',
-                                       '-m', '50'])
-        average_step_sync_time, average_throughput, total_throughput, \
-            runtime_all, runtime_main, runtime_loop  = out.strip('()\n\"').split(', ')
-        average_step_sync_time_list.append(float(average_step_sync_time))
-        average_throughput_list.append(float(average_throughput))
-        total_throughput_list.append(float(total_throughput))
-        runtime_all_list.append(float(runtime_all))
-        runtime_main_list.append(float(runtime_main))
-        runtime_loop_list.append(float(runtime_loop))
-    w.writerow([lpus,
-                np.average(average_step_sync_time_list),
-                np.average(average_throughput_list),
-                np.average(total_throughput_list),
-                np.average(runtime_all_list),
-                np.average(runtime_main_list),
-                np.average(runtime_loop_list)])
+script_name = 'timing_demo.py'
+trials = 3
+
+f = open(out_file, 'w', 0)
+w = csv.writer(f)
+for spikes in xrange(250, 7000, 1000):
+    for lpus in xrange(2, 9):
+        for i in xrange(trials):
+            out = subprocess.check_output(['srun', '-n', '1', '-c', str(lpus),
+                                           '-p', 'huxley',
+                                           'python', script_name,
+                                           '-u', str(lpus), '-s', str(spikes/(lpus-1)),
+                                           '-g', '0', '-m', '50'])
+            average_step_sync_time, runtime_all, runtime_main, \
+                runtime_loop  = out.strip('()\n\"').split(', ')
+            w.writerow([lpus, spikes, average_step_sync_time,
+                        runtime_all, runtime_main, runtime_loop])
+f.close()
