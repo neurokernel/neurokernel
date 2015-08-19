b6e012df919d4f3ce23820195d7bfc2426f3d035
Lev Givon <lev@columbia.edu>
2015-08-19 12:41:06 -0400

    Update retina_lamina demo to work with MPI-based code.

--- examples/retina_lamina/visualize_output.py
+++ examples/retina_lamina/visualize_output.py
@@ -50,8 +50,8 @@ V.add_plot(conf_L1, 'Lamina', 'L1')
 V.fontsize = 22
 V.fps = 5
 V.update_interval = 50
-V.out_filename = 'vision_output.mp4'
-V.codec = 'mpeg4'
+V.out_filename = 'vision_output.avi'
+V.codec = 'libtheora'
 V.dt = 0.0001
-V.FFMpeg = True      # change to False to use LibAV fork instead( default on UBUNTU)
+V.FFMpeg = False      # change to False to use LibAV fork instead( default on UBUNTU)
 V.run()
