fccc1bc8db0480ac7abd9df490fe27c07bb900de
Lev Givon <lev@columbia.edu>
2016-02-09 09:28:33 -0500

    Move neurokernel/LPU/* to neurodriver repo.

--- neurokernel/LPU/synapses/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-import glob
-
-__all__ = [ os.path.basename(f)[:-3] for f in \
-            glob.glob(os.path.dirname(__file__)+"/*.py") if \
-            not '__init__' in os.path.basename(f)[:-3]]
