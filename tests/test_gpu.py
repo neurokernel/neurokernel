c6be1f864efce8be1512f482678f8dbfb3feedbc
Lev Givon <lev@columbia.edu>
2016-03-19 22:50:04 -0400

    Really add unit test.

--- tests/test_gpu.py
+++ tests/test_gpu.py
@@ -18,7 +18,22 @@ class test_gpu(TestCase):
         ind_gpu = gpuarray.to_gpu(ind)
 
         result = gpu.get_by_inds(src_gpu, ind_gpu)
-        np.allclose(result, src[ind])
+        assert np.allclose(result, src[ind])
+
+    def test_set_by_inds(self):
+        # Set specified entries in dest_gpu to src_gpu:
+        dest_gpu = gpuarray.to_gpu(np.arange(5, dtype=np.float32))
+        ind = gpuarray.to_gpu(np.array([0, 2, 4]))
+        src_gpu = gpuarray.to_gpu(np.array([1, 1, 1], dtype=np.float32))
+        gpu.set_by_inds(dest_gpu, ind, src_gpu, 'dest')
+        assert np.allclose(dest_gpu.get(), np.array([1, 1, 1, 3, 1], dtype=np.float32))
+
+        # Set dest_gpu to specified entries in src_gpu:
+        dest_gpu = gpuarray.to_gpu(np.zeros(3, dtype=np.float32))
+        ind = gpuarray.to_gpu(np.array([0, 2, 4]))
+        src_gpu = gpuarray.to_gpu(np.arange(5, dtype=np.float32))
+        gpu.set_by_inds(dest_gpu, ind, src_gpu, 'src')
+        assert np.allclose(dest_gpu.get(), np.array([0, 2, 4], dtype=np.float32))
 
 if __name__ == '__main__':
     main()
